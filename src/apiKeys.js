200d2119cce78d5d9bd3c62892fd23efdb1c2694
sarmistha satrusallya <satrusallyasarmistha@gmail.com>
2023-11-20 09:10:44 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "461d8c30ed051571e713e530229a08d1",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
